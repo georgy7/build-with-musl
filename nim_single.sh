ba3eadaca766ca2af010f083e886c06acd330b00
Георгий У <httpcatharsis@gmail.com>
2017-06-05 01:04:58 +0300

    Update nim_single.sh

--- nim_single.sh
+++ nim_single.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+# Written in 2017 by Georgy Ustinov.
+# License: СС0.
+# http://creativecommons.org/publicdomain/zero/1.0/
+#
+# This is a part of the Build with Musl tutorial.
+# https://github.com/georgy7/build-with-musl
+
 SOURCE=$1
 RESULT=$2
 
